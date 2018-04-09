ffea1c6cb35808186f0d0a5e8eb1252175d8da37
Ty Thompson <tylermoko@gmail.com>
2018-04-09 13:32:26 -0400

    changing title size

--- src/Work.js
+++ src/Work.js
@@ -11,15 +11,15 @@ class Work extends Component {
       </h3>
       </div>
       <hr />
-        <div id="v" classname="gallery__img">(1963) V.</div>
-        <div id="clot" classname="gallery__img">(1966) The Crying of Lot 49</div>
-        <div id="grainbow" classname="gallery__img">(1973) Gravity's Rainbow</div>
-        <div id="slearner" classname="gallery__img">(1984) Slow Learner</div>
-        <div id="vland" classname="gallery__img">(1990) Vineland</div>
-        <div id="mdixon" classname="gallery__img">(1997) Mason & Dixon</div>
-        <div id="atday" classname="gallery__img">(2006) Against the Day</div>
-        <div id="ivice" classname="gallery__img">(2009) Inherent Vice</div>
-        <div id="bedge" classname="gallery__img">(2013) Bleeding Edge</div>
+        <div id="v" className="gallery__img">(1963) V.</div>
+        <div id="clot" className="gallery__img">(1966) The Crying of Lot 49</div>
+        <div id="grainbow" className="gallery__img">(1973) Gravity's Rainbow</div>
+        <div id="slearner" className="gallery__img">(1984) Slow Learner</div>
+        <div id="vland" className="gallery__img">(1990) Vineland</div>
+        <div id="mdixon" className="gallery__img">(1997) Mason & Dixon</div>
+        <div id="atday" className="gallery__img">(2006) Against the Day</div>
+        <div id="ivice" className="gallery__img">(2009) Inherent Vice</div>
+        <div id="bedge" className="gallery__img">(2013) Bleeding Edge</div>
       </div>
     );
   }
