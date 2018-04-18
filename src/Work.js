05af122fdf5ec6679127e889b74e938316748cff
Ty Thompson <tylermoko@gmail.com>
2018-04-18 10:24:43 -0400

    adding links to book gallery

--- src/Work.js
+++ src/Work.js
@@ -12,15 +12,15 @@ class Work extends Component {
       </div>
       <hr />
       <div className="gallery__container">
-          <div id="v" className="gallery__img"></div>
-          <div id="clot" className="gallery__img"></div>
-          <div id="grainbow" className="gallery__img"></div>
-          <div id="slearner" className="gallery__img"></div>
-          <div id="vland" className="gallery__img"></div>
-          <div id="mdixon" className="gallery__img"></div>
-          <div id="atday" className="gallery__img"></div>
-          <div id="ivice" className="gallery__img"></div>
-          <div id="bedge" className="gallery__img"></div>
+          <a href="https://www.penguinrandomhouse.com/books/312578/v-by-thomas-pynchon/9781101594568" target="_blank"><div id="v" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312579/the-crying-of-lot-49-by-thomas-pynchon/9781101594605" target="_blank"><div id="clot" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/299800/gravitys-rainbow-by-thomas-pynchon/" target="_blank"><div id="grainbow" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312580/slow-learner-by-thomas-pynchon" target="_blank"><div id="slearner" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312581/vineland-by-thomas-pynchon" target="_blank"><div id="vland" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312582/mason-and-dixon-by-thomas-pynchon" target="_blank"><div id="mdixon" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312583/against-the-day-by-thomas-pynchon" target="_blank"><div id="atday" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/312584/inherent-vice-by-thomas-pynchon" target="_blank"><div id="ivice" className="gallery__img"></div></a>
+          <a href="https://www.penguinrandomhouse.com/books/306761/bleeding-edge-by-thomas-pynchon" target="_blank"><div id="bedge" className="gallery__img"></div></a>
         </div>
       </div>
     );
